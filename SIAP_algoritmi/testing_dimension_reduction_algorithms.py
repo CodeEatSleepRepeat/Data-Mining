dd1f56e28a1bea5bfea9e03a59cc6ae79bf983c5
Sale1996 <ftnmejl96@gmail.com>
2020-02-26 20:18:57 +0100

    Istestiranih jos par metoda redukcije dimenzionalnosti i krenuta akcija popunjavanja nedostajucih vrednosti u datasetovima koji nisu u opsegu 1990-2017

--- SIAP_algoritmi/testing_dimension_reduction_algorithms.py
+++ SIAP_algoritmi/testing_dimension_reduction_algorithms.py
@@ -44,7 +44,7 @@ Povratna vrednost je malo cudnolika, ali vraca nekakvu matricu (mozda redukovanu
 '''
 x_train_without_nan = x_train.fillna(x_train.mean())
 y_train_without_nan = y_train.fillna(y_train.mean())
-random_forest_data_set = dimension_reduction_algorithms.random_forest(x_train_without_nan, y_train_without_nan, False, 1, 10)
+# random_forest_data_set = dimension_reduction_algorithms.random_forest(x_train_without_nan, y_train_without_nan, False, 1, 10)
 # print(random_forest_data_set)
 
 
@@ -57,7 +57,7 @@ ne prihvata NaN vrednosti
 vraca dataset sa 6 kolona kao sto smo naveli da zelimo da ima, valjda je uradio neku redukciju :D
 '''
 x_train_without_nan = x_train.fillna(x_train.mean())
-pca_data_set = dimension_reduction_algorithms.principal_component_analysis_pca(pd.get_dummies(x_train_without_nan), 6)
+# pca_data_set = dimension_reduction_algorithms.principal_component_analysis_pca(pd.get_dummies(x_train_without_nan), 6)
 # print(pca_data_set)
 
 
@@ -70,6 +70,63 @@ ne prihvata NaN vrednosti
 stampanje grafikona pravi neku gresku treba jos ispitati
 kao rezultat vratio je matricu sa brojem kolona koliko smo mi prosledili za n_components
 '''
-factor_analysis = dimension_reduction_algorithms.factor_analysis(pd.get_dummies(x_train_without_nan), 20, False)
+# factor_analysis = dimension_reduction_algorithms.factor_analysis(pd.get_dummies(x_train_without_nan), 20, False)
 # print(factor_analysis.shape)
 
+
+'''
+BACKWARD FEATURE SELECTION
+
+Ne prihvata stringove u tabeli, znaci samo brojeve, one hot encoding od x_train.. ali uz to kao i svi drugi 
+zahteva da nema NaN vrednosti...
+
+iz nekog razloga vraca dataset sa svim nulama
+'''
+# backward_feature_selection_data_set = dimension_reduction_algorithms.backward_feature_selection(pd.get_dummies(x_train_without_nan), y_train_without_nan, 6)
+# print(backward_feature_selection_data_set)
+
+
+'''
+FORWARD FEATURE SELECTION
+
+Trazi da dataset nema kategorijske vrednosti niti NaN vrednosti..
+Vraca kao rezultat nazive kolona koje je on izabrao..
+'''
+# forward_feature_selection_data_set = dimension_reduction_algorithms.forward_feature_selection(pd.get_dummies(x_train_without_nan), y_train_without_nan, 0)
+# print(forward_feature_selection_data_set)
+
+'''
+AUTO ENCODER
+
+kao rezultat vraca nekakav dataset sa brojem kolona koje smo mi prosledili u funkciju
+'''
+# auto_encoder_data_set = dimension_reduction_algorithms.autoencoder_dimension_reduction(pd.get_dummies(x_train_without_nan), 10)
+# print(auto_encoder_data_set.shape)
+
+
+'''
+LINEAR DISCRIMINANT ANALYSIS
+
+Dobijam error : ValueError: Unknown label type: (array([0.64953926, 2.00401235, 2.1730623 , ..., 2.79262667, 2.79262667,
+       2.79262667]),) 
+       
+       u pozivu .fit funkcije ovog algoritma
+'''
+# linear_discriminant_analysis_data_set = dimension_reduction_algorithms.linear_discriminant_analysis(pd.get_dummies(x_train_without_nan), y_train_without_nan, 10)
+
+
+'''
+IDEPEDENT COMPONENT ANALYSIS - ICA
+
+vraca dataset sa brojem kolona (feature-a) koliko smo mu prosledili u funkciju
+'''
+# ica_data_set = dimension_reduction_algorithms.idepedent_component_analysis_ica(pd.get_dummies(x_train_without_nan), 10, False)
+# print(ica_data_set.shape)
+
+'''
+UNIFORM MANIFOLD APPROXIMATION AND PROJECTION
+
+ok i ovo izbacuje dataset sa izabranim brojem kolona, ali izbacuje i neke warninge pre toga u crvenoj boji...
+'''
+umap_data_set = dimension_reduction_algorithms.uniform_manifold_approximation_and_projection(pd.get_dummies(x_train_without_nan), 10, False)
+print(umap_data_set.shape)
